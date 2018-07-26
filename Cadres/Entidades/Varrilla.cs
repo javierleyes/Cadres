ef7bf6b59beb43b03bef220bdce936656cbfd17f
javier_PC <javier.nunezleyes@gmail.com>
2018-07-26 00:03:11 -0300

    Creo entidad varrilla

---
+++ Cadres/Entidades/Varrilla.cs
@@ -0,0 +1,10 @@
+﻿namespace Entidades
+{
+    public class Varrilla
+    {
+        public long Id { get; set; }
+        public string Nombre { get; set; }
+        public decimal Precio { get; set; }
+        public int Cantidad { get; set; }
+    }
+}
