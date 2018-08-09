9fdfdaaa5d1f77c0a3144f092c46041e0782964a
javier_PC <javier.nunezleyes@gmail.com>
2018-08-08 23:14:56 -0300

    Agrego DTO de varilla y pedido

---
+++ Cadres/Entidades/DTOs/PedidoDTO.cs
@@ -0,0 +1,24 @@
+﻿using Base;
+using System;
+
+namespace Entidades.DTOs
+{
+    public class PedidoDTO
+    {
+        public long Id { get; set; }
+
+        public decimal Ancho { get; set; }
+
+        public decimal Largo { get; set; }
+
+        public VarillaDTO Varilla { get; set; }
+
+        public string Observaciones { get; set; }
+
+        public DateTime Fecha { get; set; }
+
+        public decimal Precio { get; set; }
+
+        public Estados.EstadoPedido Estado { get; set; }
+    }
+}
