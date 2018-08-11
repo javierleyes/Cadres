01937919fac9172d347637a30e5a3f9fa6188820
javier_PC <javier.nunezleyes@gmail.com>
2018-08-11 17:54:32 -0300

    Crear DAO Pedido #18

---
+++ Cadres/DAOs/Implements/PedidoDAO.cs
@@ -0,0 +1,12 @@
+﻿using Entidades;
+using System.Data.Entity;
+
+namespace DAOs.Implements
+{
+    public class PedidoDAO : GenericDAO<Pedido>
+    {
+        public PedidoDAO(DbContext dbContext) : base(dbContext)
+        {
+        }
+    }
+}
