6b4d13b5eeac03e94812b54bfd7f56a4341e26a2
Blaz77 <lugusor@gmail.com>
2018-08-18 19:59:19 -0300

    Completo interfaces de servicios

--- Cadres.Core/Services/Interfaces/Inventario/IVarillaService.cs
+++ Cadres.Core/Services/Interfaces/Inventario/IVarillaService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Services.DTO.Inventario;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,6 +7,20 @@ namespace Services.Interfaces.Inventario
 {
     public interface IVarillaService
     {
+        void Insert(VarillaDTO varillaDTO);
 
+        VarillaDTO GetDTOById(int id);
+
+        IList<VarillaDTO> GetDTOAll();
+
+        void SetCantidad(VarillaDTO varillaDTO);
+
+        void SetPrecio(VarillaDTO varillaDTO);
+
+        void DarDeBaja(VarillaDTO varillaDTO);
+
+        IList<VarillaDTO> GetByDisponibilidad(bool estaDisponible);
+
+        IList<VarillaDTO> GetByAncho(decimal ancho);
     }
 }
--- Cadres.Core/Services/Interfaces/Operaciones/IPedidoService.cs
+++ Cadres.Core/Services/Interfaces/Operaciones/IPedidoService.cs
@@ -1,11 +1,19 @@
-﻿using System;
+﻿using Services.DTO.Operaciones;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using Utils.Enums;
 
 namespace Services.Interfaces.Operaciones
 {
     public interface IPedidoService
     {
+        void Insert(PedidoDTO pedidoDTO);
 
+        PedidoDTO GetDTOById(int id);
+
+        IList<PedidoDTO> GetDTOAll();
+
+        IList<PedidoDTO> GetByEstado(Estados.EstadoPedido estado);
     }
 }
