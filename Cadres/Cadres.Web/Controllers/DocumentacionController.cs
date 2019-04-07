28f643c702fd5bd99c04d838fe5832b2192de3e0
javierleyes <javier.nunezleyes@gmail.com>
2019-04-07 19:45:14 -0300

    View documentacion de funciones disponibles (#75)

---
+++ Cadres/Cadres.Web/Controllers/DocumentacionController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+
+namespace Cadres.Web.Controllers
+{
+    public class DocumentacionController : Controller
+    {
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
